5d9fe17c6833077cc5bfa4cd8c5811d67dd09b26
John Mercouris <john@mercouris.email>
2020-03-16 17:38:52 +0100

    interface.h: expose C functions (avoid name mangling)

--- source/interface.h
+++ source/interface.h
@@ -12,6 +12,25 @@
 #endif
 
 EXTERNC void* initialize();
+EXTERNC void* newQApplication(int argc, char** argv);
+EXTERNC int applicationExec(void* application);
+EXTERNC void* newQWidget();
+EXTERNC void* newQVBoxLayout();
+EXTERNC void* newQWebEngineView();
+EXTERNC void webEngineViewLoad(void* webEngineView, char* url);
+EXTERNC void* webEngineViewPage(void* webEngineView);
+EXTERNC void webEnginePageRunJavaScript(void* webEnginePage, char* javascript);
+EXTERNC void layoutAddWidget(void* layout, void* widget);
+EXTERNC void widgetSetLayout(void* widget, void* layout);
+EXTERNC void widgetShow(void* widget);
+EXTERNC void widgetResize(void* widget, int width, int height);
+EXTERNC void widgetSetFixedHeight(void* widget, int height);
+EXTERNC void windowSetWindowTitle(void* window, char* title);
+EXTERNC void layoutSetContentsMargins(void* layout, int left, int top, int right, int bottom);
+EXTERNC void layoutSetSpacing(void* layout, int spacing);
+EXTERNC void layoutInsertWidget(void* layout, int index, void* widget);
 
 #undef EXTERNC
+
+
 
